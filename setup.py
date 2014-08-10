711891401a121aa2eb7ee10cc8c86af2cae05fdb
btweinstein <btweinstein@gmail.com>
2014-08-10 15:14:12 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-ternary',
+    version='0.1',
+    packages=['ternary'],
+    url='',
+    license='',
+    author='Bryan Weinstein',
+    author_email='bweinstein@seas.harvard.edu',
+    description=''
+)
