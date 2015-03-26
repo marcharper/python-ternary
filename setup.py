0a0f308bc1d0d1f07d5283a4f5849a9275a721e6
bryan <btweinstein@gmail.com>
2015-03-26 17:24:47 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-ternary',
+    version='0.1',
+    packages=['ternary'],
+    url='https://github.com/btweinstein/python-ternary',
+    license='see license.txt',
+    author='',
+    author_email='bweinstein@seas.harvard.edu',
+    description='Ternary plots in python'
+)
